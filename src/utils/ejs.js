6a4afc3948af97920ad4ed67706477dedde80d02
Darek Kay <darekkay@gmail.com>
2019-05-03 17:27:21 +0200

    Split data sources per language

--- src/utils/ejs.js
+++ src/utils/ejs.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 
 const renderTemplate = ({ path, fileName = "template.ejs", item }) => {
   const template = readFileSync(join(path, fileName), "utf-8");
-  return ejs.render(template, item);
+  return ejs.render(template, item, { filename: join(path, fileName) });
 };
 
 module.exports = renderTemplate;
