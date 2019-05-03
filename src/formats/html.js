bd1b39b3f681757d9254994bc47c04eb54a5f2f7
Darek Kay <darekkay@gmail.com>
2019-05-03 17:30:59 +0200

    Add RSS subscription link to the HTML version

--- src/formats/html.js
+++ src/formats/html.js
@@ -1,4 +1,5 @@
 const renderTemplate = require("../utils/ejs");
+const { baseUrl } = require("../config");
 
 const generateHTML = ({ source, entry, renderer }) => {
   const rendered = renderer(entry);
@@ -10,10 +11,12 @@ const generateHTML = ({ source, entry, renderer }) => {
       path: __dirname,
       fileName: "html-scaffold.ejs",
       item: {
+        id: source.id,
         content: rendered.content,
         title: source.title,
         name: rendered.title,
         source: rendered.url,
+        rssUrl: `${baseUrl}/rss/${source.id}.xml`,
         license: source.license,
         copyright: source.copyright
       }
