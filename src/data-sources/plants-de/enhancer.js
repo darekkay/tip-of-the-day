b846390e10f8d3391ac7c7c4568697d1ec5bac50
Darek Kay <darekkay@gmail.com>
2019-07-17 20:41:23 +0200

    Update dependencies

--- src/data-sources/plants-de/enhancer.js
+++ src/data-sources/plants-de/enhancer.js
@@ -7,9 +7,7 @@ const enhance = entry =>
   axios
     .get(
       encodeURI(
-        `https://www.smagy.de/service/plantInfo.php?in_data_type=1&in_data=${entry}&out_data=3&cust_id=${
-          config.api.smagy
-        }`
+        `https://www.smagy.de/service/plantInfo.php?in_data_type=1&in_data=${entry}&out_data=3&cust_id=${config.api.smagy}`
       )
     )
     .then(response => {
--- src/data-sources/plants-de/template.js
+++ src/data-sources/plants-de/template.js
@@ -2,9 +2,7 @@ const renderItemContent = require("../../utils/ejs");
 
 const renderItem = data => ({
   title: data.name,
-  url: `https://www.smagy.de/index.php?func=plant&task=showPlant&taskID=${
-    data.id
-  }`,
+  url: `https://www.smagy.de/index.php?func=plant&task=showPlant&taskID=${data.id}`,
   content: renderItemContent({ path: __dirname, item: data })
 });
 
